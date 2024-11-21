e25837c374e4534f6a02a5b7c5b920c7f689cc61
Sampleee1 <fred.gamer.br1@gmail.com>
2024-11-20 21:11:14 -0300

    Start the project and add the rules

--- main.py
+++ main.py
@@ -1,1 +1,91 @@
-asdsadas
+import os
+import time
+
+
+def verificacao():
+
+    while True:
+        global inicio
+        if inicio not in ["1", "2", "3", "4"]:
+            inicio = input(str("""Opção invalida, digite novamente! 
+    ==> """)) 
+        else: 
+            os.system('cls')
+            opcoes()
+            break
+
+
+def caminhos():
+    global inicio
+    inicio = input(str("""
+    -------------------------------------
+    O que deseja fazer?
+    (1) Jogar
+    (2) Ver regras
+    (3) Selecionar dificuldade
+    (4) Sair
+        
+    ==> """))
+    verificacao()
+
+def opcoes():
+    global inicio
+    if inicio == "1": FuncaoUm()
+    elif inicio == "2": FuncaoDois()
+    elif inicio == "3": FuncaoTres()
+    elif inicio == "4": print("Adeus!"); time.sleep(1)
+
+
+def FuncaoUm():
+    0
+
+
+def FuncaoDois():
+    print("Link do PDF das regras: https://www.bauru.unesp.br/Home/Div.Tec.Biblioteca/bd-manual-uno.pdf")
+    time.sleep(1)
+    caminhos()
+
+
+def FuncaoTres():
+    0
+
+
+def FuncaoQuatro():
+    0
+
+
+inicio = input(str((""" 
+              _____                    _____                   _______         
+             /\    \                  /\    \                 /::\    \        
+            /::\____\                /::\____\               /::::\    \       
+           /:::/    /               /::::|   |              /::::::\    \      
+          /:::/    /               /:::::|   |             /::::::::\    \     
+         /:::/    /               /::::::|   |            /:::/~~\:::\    \    
+        /:::/    /               /:::/|::|   |           /:::/    \:::\    \   
+       /:::/    /               /:::/ |::|   |          /:::/    / \:::\    \  
+      /:::/    /      _____    /:::/  |::|   | _____   /:::/____/   \:::\____\ 
+     /:::/____/      /\    \  /:::/   |::|   |/\    \ |:::|    |     |:::|    |
+    |:::|    /      /::\____\/:: /    |::|   /::\____\|:::|____|     |:::|    |
+    |:::|____\     /:::/    /\::/    /|::|  /:::/    / \:::\    \   /:::/    / 
+     \:::\    \   /:::/    /  \/____/ |::| /:::/    /   \:::\    \ /:::/    /  
+      \:::\    \ /:::/    /           |::|/:::/    /     \:::\    /:::/    /   
+       \:::\    /:::/    /            |::::::/    /       \:::\__/:::/    /    
+        \:::\__/:::/    /             |:::::/    /         \::::::::/    /     
+         \::::::::/    /              |::::/    /           \::::::/    /      
+          \::::::/    /               /:::/    /             \::::/    /       
+           \::::/    /               /:::/    /               \::/____/        
+            \::/____/                \::/    /                 ~~              
+             ~~                       \/____/                                         
+    -----------------------------------------------------------------------------
+    Seja bem-vindo ao UNO, desenvolvido por Fred, Nata e Miguel Yoshida. :)
+
+    O que deseja fazer?
+    (1) Jogar
+    (2) Ver regras
+    (3) Selecionar dificuldade
+    (4) Sair
+
+    ==> """)))
+
+verificacao()
+
